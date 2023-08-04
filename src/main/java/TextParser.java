17957da33ae646330235612260363ab64a2cbc56
Richard Ropac <Richard.Ropac@tlgcohort.com>
2023-08-04 12:27:10 -0500

    Finalized TextParser with Look, AB#232

--- src/main/java/TextParser.java
+++ src/main/java/TextParser.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,43 +7,64 @@ public class TextParser {
     public static List<String> extractVerbsAndNouns(String input) {
 
         List<String> verbsAndNouns = new ArrayList<>();
-        List<String> defaultValue = new ArrayList<>(Arrays.asList("I don't understand, please try again.  Type 'Help' for a list of commands."));
 
         // Define regular expressions for verbs and nouns
-        String verbRegex = "\\b(?:go|attack|take|look|inspect|quit|help)\\b";
+        String verbRegex = "\\b(?:go|attack|take|look|inspect|quit|move|help)\\b";
         String nounRegex = "\\b(?:north|south|east|west|vampire|crystalball|crown|spirit|stairs)\\b";
 
         Pattern verbPattern = Pattern.compile(verbRegex, Pattern.CASE_INSENSITIVE);
         Pattern nounPattern = Pattern.compile(nounRegex, Pattern.CASE_INSENSITIVE);
 
-        try{
-            Matcher verbMatcher = verbPattern.matcher(input);
-            Matcher nounMatcher = nounPattern.matcher(input);
 
-            // Commands Extractor to String to List
-            String stringOfCommands = verbRegex.replace("\\b(?:", "").replace(")\\b", "");
-            String [] commands = stringOfCommands.split("\\|");
-            List<String> commandList = Arrays.asList(commands);
+        Matcher verbMatcher = verbPattern.matcher(input.toLowerCase());
+        Matcher nounMatcher = nounPattern.matcher(input.toLowerCase());
 
-            // Extract verb
-            while (verbMatcher.find()) {
-                String verb = verbMatcher.group();
-                verbsAndNouns.add(verb);
-            }
+        //Create Return Statements
 
-            // Extract noun
-            while (nounMatcher.find()) {
-                String noun = nounMatcher.group();
-                verbsAndNouns.add(noun);
-            }
-            if (verbsAndNouns.get(0).equalsIgnoreCase("help")) {
-                return commandList;
-            } else {
-                return verbsAndNouns;
-            }
-        } catch (Exception e) {
-            return defaultValue;
+        // Commands Extractor to String to List
+        String stringOfCommands = verbRegex.replace("\\b(?:", "").replace(")\\b", "");
+        //String [] commands = stringOfCommands.split("\\|");
+
+        // Look for invalid Noun
+        String badLook = "The item you entered is not valid.  Please try again";
+
+        // Move for invalid Noun
+        String badMove = "The location you entered to move to is not valid.  Please try again";
+
+        // Invalid Command
+        String badCommand = "I don't understand, please try again.  Type 'Help' for a list of commands.";
+
+
+        // Extract verb
+        while (verbMatcher.find()) {
+            String verb = verbMatcher.group();
+            verbsAndNouns.add(verb);
         }
+
+        // Extract noun
+        while (nounMatcher.find()) {
+            String noun = nounMatcher.group();
+            verbsAndNouns.add(noun);
+        }
+
+        if (verbsAndNouns.size() == 1 && !verbsAndNouns.isEmpty()) {
+            switch (verbsAndNouns.get(0)) {
+                case "help":
+                    verbsAndNouns.set(0, stringOfCommands);
+                    break;
+                case "look":
+                    verbsAndNouns.set(0, badLook);
+                    break;
+                case "move":
+                    verbsAndNouns.set(0, badMove);
+                    break;
+                default:
+                    break;
+            }
+        } else if (verbsAndNouns.isEmpty()){
+            verbsAndNouns.add(badCommand);
+        }
+        return verbsAndNouns;
     }
 
 
@@ -65,7 +83,7 @@ public class TextParser {
 
             System.out.println(response);
 
-            // Check for game over condition or other exit conditions
+            //Check for game over condition or other exit conditions
             if (response.get(0).equalsIgnoreCase("quit")) {
                 System.out.println("Exiting the game. Goodbye!");
                 break;
