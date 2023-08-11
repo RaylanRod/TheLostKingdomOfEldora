99514c79011d37b76531c9126f1e4b8450e971e0
Richard Ropac <Richard.Ropac@tlgcohort.com>
2023-08-10 23:06:14 -0500

    Players can now see where they are on map, AB#258

--- src/main/java/DisplayMethods.java
+++ src/main/java/DisplayMethods.java
@@ -18,6 +18,23 @@ public class DisplayMethods {
         }
     }
 
+    public static void printHeader() {
+        System.out.println("Character:\n" + Main.player);
+        System.out.println("Description:\n" + Rooms.getRoomById(Main.player.getCurrentRoom()).getDescription() + "\n");
+    }
+
+    public static void printRoomItems() {
+        if (Rooms.getRoomById(Main.player.getCurrentRoom()).getItems().size() > 0){
+            System.out.print("Things in the Room: | ");
+            for (int i=0; i < Rooms.getRoomById(Main.player.getCurrentRoom()).getItems().size(); ++i ) {
+                System.out.print(Rooms.getRoomById(Main.player.getCurrentRoom()).getItems().get(i).get("name") + " | ");
+            }
+            System.out.println("\n");
+        } else {
+            System.out.println("Things in the Room: | Nothing in the room |\n");
+        }
+    }
+
     public static void printTextFile(String fileName) {
         // Prints the opening splash screen
         //noinspection ConstantConditions
@@ -25,6 +42,61 @@ public class DisplayMethods {
             String line;
             while ((line = br.readLine()) != null) {
                 System.out.println(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.print("Press any key to continue...");
+        // Wait for the player to press Enter before continuing
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+    }
+
+    public static void printTextMap() {
+
+        String path = "textFiles/Castle_Map.txt";
+        String spaces32 = new String(new char[32]).replace('\0', ' ');
+
+        int inRoom = Main.player.getCurrentRoom();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(DisplayMethods.class.getClassLoader().getResourceAsStream(path)))) {
+            String line;
+            int i = 1;
+            while ((line = br.readLine()) != null) {
+                switch (i) {
+                    case 9:
+                        if (inRoom == 9)
+                            System.out.println(spaces32 + "##     X    ##");
+                        break;
+                    case 18:
+                        if (inRoom == 10)
+                            System.out.println("##         X              ##                  ##                          ##");
+                        if (inRoom == 5)
+                            System.out.println("##                        ##         X        ##                          ##");
+                        if (inRoom == 8)
+                            System.out.println("##                        ##                  ##            X             ##");
+                        break;
+                    case 26:
+                        if (inRoom == 2)
+                            System.out.println("##                    ##             X             ##                     ##");
+                        if (inRoom == 6)
+                            System.out.println("##        X           ##                           ##                     ##");
+                        if (inRoom == 7)
+                            System.out.println("##                    ##                           ##        X            ##");
+                        break;
+                    case 35:
+                        if (inRoom == 4)
+                            System.out.println("##    #   X  #                                             #      #       ##");
+                        if (inRoom == 1)
+                            System.out.println("##    #      #                       X                     #      #       ##");
+                        if (inRoom == 3)
+                            System.out.println("##    #      #                                             #   X  #       ##");
+                        break;
+                    default:
+                        System.out.println(line);
+                        break;
+            }
+                i++;
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
