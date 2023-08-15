a3f449fbfae4f9389d641e85825313f4653165f8
RaylanRod <Raylan.Rodriguez@tlglearning.com>
2023-08-15 13:59:10 -0700

    changed the command to change location from move to go AB#683

--- src/main/java/TextParser.java
+++ src/main/java/TextParser.java
@@ -10,7 +10,7 @@ public class TextParser extends Colors{
 
         // Define regular expressions for verbs and noun
       
-        String verbRegex = "\\b(?:adjust|attack|drop|get|help|look|map|move|play|quit|save|stop|talk|volume)\\b";
+        String verbRegex = "\\b(?:adjust|attack|drop|get|help|look|map|go|play|quit|save|stop|talk|volume)\\b";
         String nounRegex = "\\b(?:elder vampire|interactive painting|librarian|enigma|crystal ball with a trapped spirit|bardleston|music|fx|north|south|east|west|up|down|vampire|crystalball|royal crown piece left|spirit|stairs|rock|royal seal|royal crown piece right|royal crown piece middle|shadow cloak|elixir of restoration|royal crown piece back|ancient amulet of binding|1|2|3|4|5|6|7|8|9|10)\\b";
 
         Pattern verbPattern = Pattern.compile(verbRegex, Pattern.CASE_INSENSITIVE);
@@ -78,7 +78,7 @@ public class TextParser extends Colors{
                 case "look":
                     verbsAndNouns.set(0, badLook);
                     break;
-                case "move":
+                case "go":
                     verbsAndNouns.set(0, badMove);
                     break;
                 case "get":
